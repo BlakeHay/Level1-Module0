6ea91e1d607eac9abd99ee020a73a5da5e9bbe41
BlakeHay <blake.christian.hay@gmail.com>
2025-05-26 23:59:47 +0000

    Starting Level One Mon May 26 2025

--- src/_01_methods/_1_houses/Houses.java
+++ src/_01_methods/_1_houses/Houses.java
@@ -1,11 +1,36 @@
 package _01_methods._1_houses;
+import java.awt.Color;
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2021
  *    Level 1
  */
+import org.jointheleague.graphical.robot.Robot;
 public class Houses {
 		
 	public void run() {
+	
+		houses(10);
 		// Check the recipe to find out what code to put here
+
 	}
+	public void houses(int numberOfHouses){
+		Robot rob = new Robot();
+	for(int i = 0; i < numberOfHouses; i++){
+	
+	rob.miniaturize();
+	rob.setSpeed(10);
+	rob.setX(50);
+	rob.setY(550);
+	rob.penDown();
+	rob.move(100);
+	rob.turn(90);
+	rob.move(20);
+	rob.turn(90);
+	rob.move(100);
+	rob.turn(-90);
+	rob.setPenColor(Color.green);
+	rob.move(20);
+	}
+}
 }
--- src/_01_methods/_1_houses/HousesRunner.java
+++ src/_01_methods/_1_houses/HousesRunner.java
@@ -3,6 +3,7 @@ package _01_methods._1_houses;
  *    Copyright (c) The League of Amazing Programmers 2013-2021
  *    Level 1
  */
+
 public class HousesRunner {
 
 	public static void main(String[] args) {
